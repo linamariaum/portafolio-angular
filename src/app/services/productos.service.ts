94937df6f4c916fa93a3b53c4754d6f088b73a53
Lina Uribe <lina.uribem@udea.edu.co>
2021-08-05 17:53:16 -0500

    Creation Search component

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,23 +8,53 @@ import { InfoItem } from '../interfaces/info-item.interface';
 export class ProductosService {
   productos: InfoItem[] = [];
   cargando = true;
+  productoFiltrado: InfoItem[] = [];
 
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://portafolio-angular-aa8a8-default-rtdb.firebaseio.com/productos_idx.json')
+
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://portafolio-angular-aa8a8-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe( result => {
         this.productos = [];
         let aux = <InfoItem[]>result;
         this.productos = aux
         this.cargando = false;
+        resolve(true);
       });
+    });    
   }
 
   public cargarProductoPorId(id: string) {
     return this.http.get(`https://portafolio-angular-aa8a8-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
+
+  public buscarProducto( termino: string ) {
+    this.productoFiltrado = [];
+    if ( this.productos.length === 0 ) {
+      //Cargar productos
+      this.cargarProductos().then( () => {
+        this.filtrarProductos(termino);
+      });
+    } else {
+      //Hacer filtro
+      this.filtrarProductos(termino);      
+    }    
+  }
+
+  private filtrarProductos( termino: string) {
+    // this.productoFiltrado = this.productos.filter( producto => {
+      //   return true;
+      // });
+    this.productos.forEach ( producto => {
+      const tituloLower = producto.titulo.toLocaleLowerCase();
+      if ( producto.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productoFiltrado.push( producto )
+      }
+    });
   }
 
 }
